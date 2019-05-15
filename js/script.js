dd4c366cae129f878354413e13e684821b06aaa8
Trivikram244 <trivikram244@gmail.com>
2019-05-16 00:40:15 +0530

    update
    Update

--- js/script.js
+++ js/script.js
@@ -45,3 +45,53 @@ var weekday = new Array(7);
 $('.day').text(todayday);
 $('.date').text(todaydate);
 $('.month').text(thismonth);
+
+
+$(document).ready(function(){
+    $("select.fillters").change(function(){
+        var value = $(this).children("option:selected").val();
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+        //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+        //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+
+    });
+});
+
+$(document).ready(function(){
+
+
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
